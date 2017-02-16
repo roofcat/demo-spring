bb82d25e074564b146c1738e087b991f1b33ab40
Christian Rojas <crojas@azurian.com>
2017-02-16 09:33:59 -0300

    se agrega metodo a homeController

--- src/main/java/com/crojas/demo/controller/HomeController.java
+++ src/main/java/com/crojas/demo/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,8 +17,9 @@ import com.crojas.demo.model.service.ContactoService;
 
 
 @RestController
-@RequestMapping(value="/")
+@RequestMapping(value="/contacto")
 public class HomeController {
+	
 	
 	@Autowired
 	private ContactoService cs;
@@ -27,16 +29,6 @@ public class HomeController {
 	@ResponseBody
 	public ResponseEntity<List<Contacto>> showIndex() {
 		
-		List<Contacto> contactos = cs.findAll();
-		System.out.println( contactos.size() );
-		
-		return new ResponseEntity<List<Contacto>>( contactos, HttpStatus.OK );
-		
-	}
-	
-	@RequestMapping(value="/contacto/", method=RequestMethod.GET)
-	public ResponseEntity<Contacto> showContact() {
-		
 		Contacto contacto = new Contacto();
 		contacto.setFecha( new Date() );
 		contacto.setNombre( "Christian" );
@@ -45,7 +37,28 @@ public class HomeController {
 		
 		cs.create( contacto );
 		
-		return new ResponseEntity<Contacto>( contacto, HttpStatus.OK );
+		List<Contacto> contactos = cs.findAll();
+		
+		return new ResponseEntity<List<Contacto>>( contactos, HttpStatus.OK );
+		
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Contacto> showContact( @PathVariable Integer id  ) {
+		
+		Contacto contacto = null;
+		
+		contacto = cs.findOne( id );
+		
+		if ( contacto == null ) {
+			
+			return new ResponseEntity<Contacto>( HttpStatus.NOT_FOUND );
+			
+		} else {
+			
+			return new ResponseEntity<Contacto>( contacto, HttpStatus.OK );
+			
+		}
 		
 	}
 	
